6f2cf3ce5a06f2d5a09b93d2085c7e1aa228fd31
Michel Ferreira <michel.mf628@gmail.com>
2024-12-16 19:07:52 -0300

    making the matrix class immutable

--- src/main/java/com/michel/matrixcalculator/model/Matrix.java
+++ src/main/java/com/michel/matrixcalculator/model/Matrix.java
@@ -1,17 +1,28 @@
 package com.michel.matrixcalculator.model;
 
+import java.util.Arrays;
+
 public class Matrix {
-    private final Double[][] matix;
-    private int rows;
-    private int cols;
-    public Matrix(int rows, int cols) {
-        this.matix = new Double[rows][cols];
+    private final Double[][] matrix;
+    private final int rows;
+    private final int cols;
+    public Matrix(Double[][] values) throws IllegalArgumentException{
+        if (values == null || values.length == 0 || values[0].length == 0) {
+            throw new IllegalArgumentException("Values cannot be null or empty");
+        }
+        this.rows = values.length;
+        this.cols = values[0].length;
+        this.matrix = new Double[rows][cols];
+        //creates an immutable copy of the matrix
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = Arrays.copyOf(values[i], cols);
+        }
     }
 
-    public void setComponent(Double value, int targetRow, int targetColumn) {
-        this.matix[targetRow][targetColumn] = value;
-    }
-    public Double getComponent(int targetRow, int targetColumn) {
-        return matix[targetRow][targetColumn];
+    public Double getComponent(int targetRow, int targetColumn) throws IndexOutOfBoundsException{
+        if (targetRow < 0 || targetRow >= rows || targetColumn < 0 || targetColumn >= cols) {
+            throw new IndexOutOfBoundsException();
+        }
+        return matrix[targetRow][targetColumn];
     }
 }
