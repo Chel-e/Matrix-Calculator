881110e3c1ccfe54a5484dd63ddc2ddd35292d3f
Michel Ferreira <michel.mf628@gmail.com>
2024-12-16 19:16:35 -0300

    create MatrixTest

---
+++ src/test/java/com/michel/matrixcalculator/model/MatrixTest.java
@@ -0,0 +1,30 @@
+package com.michel.matrixcalculator.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+    Matrix matrix;
+
+    @BeforeEach
+    void setUp() {
+        Double[][] mat = { { 1D,2D },
+            { 3D, 4D}};
+        matrix = new Matrix(mat);
+    }
+
+
+
+    @DisplayName("Test if a component is equal original ")
+    @Test
+    void testGetComponent() {
+        Assertions.assertEquals(1D, matrix.getComponent(0,0));
+        Assertions.assertEquals(2D, matrix.getComponent(0,1));
+        Assertions.assertEquals(3D, matrix.getComponent(1,0));
+        Assertions.assertEquals(4D, matrix.getComponent(1,1));
+    }
+}
