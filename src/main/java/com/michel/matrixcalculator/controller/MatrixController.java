e8c3bf74ebe66b6ba483ccc73e9bf6577120f15b
Michel Ferreira <michel.mf628@gmail.com>
2024-12-26 10:44:04 -0300

    create the MatrixController

--- src/main/java/com/michel/matrixcalculator/controller/MatrixController.java
+++ src/main/java/com/michel/matrixcalculator/controller/MatrixController.java
@@ -1,5 +1,27 @@
 package com.michel.matrixcalculator.controller;
 
-public class MatrixController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.VBox;
 
+
+public class MatrixController {
+    @FXML
+    private VBox vBox;
+    @FXML
+    private void onClickSum() {
+        Label dimensionsMatrix1 = new Label("Dimensions of the first matrix");
+        TextField lineMatrix1 = new TextField();
+        lineMatrix1.setPromptText("Line");
+        TextField rowMatrix1 = new TextField();
+        rowMatrix1.setPromptText("Row");
+        Label dimensionsMatrix2 = new Label("Dimensions of the second matrix");
+        TextField lineMatrix2 = new TextField();
+        lineMatrix2.setPromptText("Line");
+        TextField rowMatrix2 = new TextField();
+        rowMatrix2.setPromptText("Row");
+        vBox.getChildren().addAll(dimensionsMatrix1, lineMatrix1, rowMatrix1, dimensionsMatrix2, lineMatrix2, rowMatrix2);
+    }
 }
